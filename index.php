6d939e0af4d5924eb971077b96fc75e50fdea25e
AlecTech <eremeevoleg@gmail.com>
2020-10-05 17:02:29 -0600

    initial commit set up folders

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$GLOBALS['pageTitle'] = 'ANIME Gimbli';
+include './templates/header.php'; ?>
+
+<p>
+  Welcome to our
+  <?php echo $GLOBALS['pageTitle']; ?>
+  page!
+</p>
+
+
+
+<?php // Show our footer.
+include './templates/footer.php';
---
+++ templates/navigation.php
@@ -0,0 +1,30 @@
+<nav>
+  <h2>Website Navigation</h2>
+  <ul>
+    <li>
+      <a href="index.php">
+        Home
+      </a>
+    </li>
+    <li>
+      <a href="calc.php">
+        Calculator
+      </a>
+    </li>
+    <li>
+      <a href="snacks.php">
+        Snacks! (OOP)
+      </a>
+    </li>
+    <li>
+      <a href="cat-facts-api.php">
+        Cat Facts (External API)
+      </a>
+    </li>
+    <li>
+      <a href="snack-search.html">
+        Snack Search (AJAX + API)
+      </a>
+    </li>
+  </ul>
+</nav>
