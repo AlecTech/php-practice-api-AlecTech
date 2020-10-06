b405ea4350d8a17fd16c8d34e2bd9e42573d3138
AlecTech <eremeevoleg@gmail.com>
2020-10-05 18:37:21 -0600

    Added API link, added echo 1st try to access data, added limited response to 5 movies, added form

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
 <?php
 $GLOBALS['pageTitle'] = 'ANIME Gimbli';
-include './templates/header.php'; ?>
+include './templates/header.php'; 
 
-<p>
-  Welcome to our
-  <?php echo $GLOBALS['pageTitle']; ?>
-  page!
-</p>
+// Retrieve data from API endpoint.
+// @link https://alexwohlbruck.github.io/cat-facts/docs/
+// https://ghibliapi.herokuapp.com/#
+$dailyAnimeResponse = file_get_contents('http://ghibliapi.herokuapp.com/films');
+// var_dump($dailyAnimeResponse);
 
+if($dailyAnimeResponse)
+{
+    $dailyAnimeFilm = json_decode($dailyAnimeResponse);
+    ?>
+    
+    <h2> Gimbli Anime MOVIES Response </h2>
+    
+    <p> <?php echo $dailyAnimeFilm[0]->title; ?> </p>
+    <?php 
+}
 
+?>
+<form action="#" method="POST">
+  <label for="limit">Enter the Amount of Movies to reflect:
+  <input type="number" id="amount" name="limit" value="3"></label>
+  <label for="fields">Enter the Type of Data needed:
+    <select id="film" name="fields">
+      <option value="people">People</option>
+      <option value="location">Location</option>
+      <option value="films">Films</option>
+      <option value="vehicles">Vehicles</option>
+    </select>
+  </label>
+  <input type="submit" value="Show Data!">
+</form>
+<?php
 
-<?php // Show our footer.
+$animeListLimit = file_get_contents('http://ghibliapi.herokuapp.com/films?limit=5');
+var_dump ($animeListLimit );
+
 include './templates/footer.php';
